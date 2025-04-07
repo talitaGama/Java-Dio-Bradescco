cbdb79ba548988dca91630cb10cb3350d14dff24
talita.Gama <talitagama0810@gmail.com>
2025-04-07 20:18:46 -0300

    teste pacotes

---
+++ Primeiro projeto Java/src/pacotes/teste/MinhaClasse.java
@@ -0,0 +1,9 @@
+package pacotes.teste;
+public class MinhaClasse {
+    public static void main(String[] args) {
+    //Primeira impressão;
+        System.out.println("Primeira impressão");
+
+    }
+    
+}
---
+++ Primeiro projeto Java/src/pacotes/teste/Variavei.java
@@ -0,0 +1,19 @@
+package pacotes.teste;
+public class Variavei {
+    public static void main(String[] args) {
+    
+        //Variáveis;
+    
+            String primeiroNome = "Talita";
+            String segundoNome = "Gama";
+    
+            String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    
+            System.out.println(nomeCompleto);
+    
+        }
+        public static String nomeCompleto(String primeiroNome, String segundoNome) {
+            return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+        }
+    
+}
